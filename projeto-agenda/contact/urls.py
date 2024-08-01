aa75119751b98863822558a84a40e49a50e1db50
Maykon Carvalho <carvalhomaykon566@gmail.com>
2024-08-01 18:49:07 -0300

    Usando um padrão comum para URLs de CRUD

--- projeto-agenda/contact/urls.py
+++ projeto-agenda/contact/urls.py
@@ -4,7 +4,12 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
-    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+
+    # Contact (CRUD)
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'), # Ler o contato
+    #path('contact/create/', views.contact, name='contact'), # Criar contato
+    #path('contact/<int:contact_id>/update/', views.contact, name='contact'), # Atualizar o contato
+    #path('contact/<int:contact_id>/delete/', views.contact, name='contact'), # Deletar o contato
 ]
