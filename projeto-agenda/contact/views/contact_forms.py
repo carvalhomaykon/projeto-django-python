dc4aa6675c35b4220a8a83430bf59203d88eaa5e
Maykon Carvalho <carvalhomaykon566@gmail.com>
2024-08-02 18:43:52 -0300

    Criando um formulário dinâmico

--- projeto-agenda/contact/views/contact_forms.py
+++ projeto-agenda/contact/views/contact_forms.py
@@ -3,29 +3,49 @@ from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 from django.core.paginator import Paginator
+from django import forms
+
+# Criar formulário no django:
+# Criar a classe
+class ContactForm(forms.ModelForm):
+    # forms.Form -> Criar um formulário do zero
+    # forms.ModelForm -> Form baseado num modelo
+    # Indicar Models
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone'
+        )
+
 
 # Create your views here.
 def create(request):
-    if request.method == 'POST':
-        print ()
-        print (request.method)
-        print (request.POST.get('first_name'))
-        print (request.POST.get('last_name'))
-        print ()
-
-    context = {
-        
-    }
-
     # Se eu acessar o site o method ==  GET
     # Se eu enviar o formulário o method == POST
+    if request.method == 'POST':
+        #print ()
+        #print (request.method)
+        #print (request.POST.get('first_name'))
+        #print (request.POST.get('last_name'))
+        #print ()
 
-    print ()
-    print (request.method)
-    print ()
+        context = {
+            'form': ContactForm(request.POST)
+        }
 
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+    
+    context = {
+        'form': ContactForm(data=request.POST)
+        }
+    
     return render(
-        request,
-        'contact/create.html',
-        context
-    )
+            request,
+            'contact/create.html',
+            context
+        )
