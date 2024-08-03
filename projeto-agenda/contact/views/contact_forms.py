e59bcf3c6289ffec24ca7de9b3479ae0136e06a5
Maykon Carvalho <carvalhomaykon566@gmail.com>
2024-08-02 23:54:29 -0300

    Criando um formulário dinâmico - Parte 2

--- projeto-agenda/contact/views/contact_forms.py
+++ projeto-agenda/contact/views/contact_forms.py
@@ -1,9 +1,11 @@
+from typing import Any
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 from django.core.paginator import Paginator
 from django import forms
+from django.core.exceptions import ValidationError
 
 # Criar formulário no django:
 # Criar a classe
@@ -16,6 +18,25 @@ class ContactForm(forms.ModelForm):
         fields = (
             'first_name', 'last_name', 'phone'
         )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        #print (cleaned_data)
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de Erro',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de Erro 2',
+                code='invalid'
+            )
+        )
+        return super().clean()
 
 
 # Create your views here.
