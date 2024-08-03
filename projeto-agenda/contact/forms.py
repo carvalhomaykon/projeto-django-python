d6031dcfedc9ee851ce18eb0b53348d45b887f80
Maykon Carvalho <carvalhomaykon566@gmail.com>
2024-08-03 19:02:48 -0300

    Validando campos
    De first_name, last_name e amail do Usuário

--- projeto-agenda/contact/forms.py
+++ projeto-agenda/contact/forms.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError
 from django import forms
 from . import models
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Criar formulário no django:
 # Criar a classe
@@ -107,4 +108,29 @@ class ContactForm(forms.ModelForm):
         return first_name
     
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email', 'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este email', code='invalid')
+            )
+
+        return email
